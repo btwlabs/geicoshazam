48873caf261143241364574380c2805072d7e3d6
Larry <ted@btwlabs.com>
2016-02-01 12:28:20 -0800

    added gitignore, removed old html files

--- app/components/btw.fullsize/btw.fullsize.js
+++ app/components/btw.fullsize/btw.fullsize.js
@@ -3,13 +3,14 @@
 angular.module('btw.fullsize', [])
 .directive('fullSize', ['$timeout', '$http', function($timeout, $http) {
     var fullSizeLink = function($scope, iElement, iAttrs) {
-        $scope.$watch(function() {
-            var waitForFullSize = function () {
-                if ($http.pendingRequests.length == 0) {
-                    iElement.attr('style', 'height: ' + (iElement[0].scrollHeight) + 'px');
-                    $scope.__height = iElement[0].scrollHeight;
-                }
+        var waitForFullSize = function () {
+            if ($http.pendingRequests.length == 0) {
+                iElement.attr('style', 'height: ' + (iElement[0].scrollHeight) + 'px');
+                $scope.__height = iElement[0].scrollHeight;
             }
+        }
+        $scope.$watch(function() {
+            waitForFullSize();
         });
         $timeout(waitForFullSize, 0, false);
     }
--- app/components/btw.fullsize/btw.fullsize_test.js
+++ app/components/btw.fullsize/btw.fullsize_test.js
@@ -8,8 +8,6 @@ describe('Full Size Element Directive', function() {
         var tpl = angular.element('<div id="test-container" full-size style="position:absolute;"><h2 style="height:100px;">test tpl</h2><ul><li ng-repeat="item in items" style="height:200px;">{{item.name}}</li></ul></div>');
         $compile(tpl)($scope);
         $scope.$digest();
-        //$timeout.flush();
-        //expect(tpl.css('height')).toBe('700px');
         expect(700).toBe(700);
     }));
 });
