d5f4b905edb5e0e74a8a6332bdacf533fefdb73e
Larry <ted@btwlabs.com>
2016-02-02 15:12:21 -0800

    adding some test fb integration

--- views/home/home.js
+++ views/home/home.js
@@ -1,17 +1,39 @@
 'use strict';
 
-angular.module('btw.home', ['ngRoute', 'dwebform', 'dview', 'dfile', 'dnode'])
+angular.module('btw.home', ['ngRoute', 'dwebform', 'dview', 'dfile', 'dnode', 'ezfb'])
 
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', 'ezfbProvider', function($routeProvider, ezfbProvider) {
     $routeProvider.when('/', {
         templateUrl: 'views/home/home.html',
         controller: 'homeCtrl',
         paged: true,
     });
+    ezfbProvider.setInitParams({
+        // This is my FB app id for plunker demo app
+        appId: '779006838893577',
+
+        // Module default is `v2.4`.
+        // If you want to use Facebook platform `v2.3`, you'll have to add the following parameter.
+        // https://developers.facebook.com/docs/javascript/reference/FB.init
+        version: 'v2.3'
+    });
 }])
 
-.controller('homeCtrl', ['$scope',
-    function($scope) {
-
+.controller('homeCtrl', ['$scope', 'ezfb',
+    function($scope, ezfb) {
+        var fbResponse = function(res) {
+            var go = 'NO';
+        }
+        $scope.shareFB = function() {
+            var res;
+            ezfb.ui({
+                method: "feed",
+                link: "http://www.bythewaylabs.com",
+                name: "A test link",
+                description: "Um, i dunno. It's a test. Enjoy.",
+                caption: "This is a caption. Enjoy it tho it sux.",
+                picture: "http://geicoshazam.bythewaylabs.com/assets/images/btwlabs-logomark_blue-accent_transbg_800x800.png"
+            }, fbResponse(res));
+        }
     }
 ]);
