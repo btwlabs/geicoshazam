d9122ddfa5e7fad17b56c278f4949ab86d72b336
Larry <ted@btwlabs.com>
2016-02-04 12:21:20 -0800

    ok need to upload img first to backend, return url, and then supply the url to fb or twitter

--- views/home/home.js
+++ views/home/home.js
@@ -1,40 +1,21 @@
 'use strict';
 
-angular.module('btw.home', ['ngRoute', 'dfile', 'ezfb'])
+angular.module('btw.home', ['ngRoute', 'dfile'])
 
-.config(['$routeProvider', 'ezfbProvider', function($routeProvider, ezfbProvider) {
+.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'views/home/home.html',
         controller: 'homeCtrl',
         paged: true,
     });
-    ezfbProvider.setInitParams({
-        // This is my FB app id for plunker demo app
-        appId: '779006838893577',
-
-        // Module default is `v2.4`.
-        // If you want to use Facebook platform `v2.3`, you'll have to add the following parameter.
-        // https://developers.facebook.com/docs/javascript/reference/FB.init
-        version: 'v2.3'
-    });
-    OAuth.initialize('U4mzG-co02w_C3N4BL1XyM9SHB0');
 }])
 
-.controller('homeCtrl', ['$scope', 'ezfb', 'dfile',
-    function($scope, ezfb, dfile) {
+.controller('homeCtrl', ['$scope', 'dfile',
+    function($scope, dfile) {
         var fbResponse = function(res) {
             var go = 'NO';
         }
         $scope.shareFB = function() {
-            /*var res;
-            ezfb.ui({
-                method: "feed",
-                link: "http://www.bythewaylabs.com",
-                name: "A test linkback to btwlabs.com",
-                description: "This is a test link that goes back to our website. Testing some FB integration",
-                caption: "This is the caption of the link.",
-                picture: "http://geicoshazam.bythewaylabs.com/assets/images/btwlabs-logomark_blue-accent_transbg_800x800.png"
-            }, fbResponse(res));*/
             OAuth.popup('facebook', {cache:true})
             .done(function(result) {
                 var img = document.getElementById("user-image");
